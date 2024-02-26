579bc79b67717593112778cb92a8928dca356903
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-02-26 14:35:57 +0500

    extension widget added with hero banner

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,22 @@ const extensionWidget = document.querySelector('.extension-widget');
 
 extensionWidget.style.display = 'none';
 
-extensionIcon.addEventListener('click', () => {
-        
-        extensionWidget.style.display = 'block';
+extensionIcon.addEventListener('click', (e) => {
+        e.stopPropagation()
+        if(extensionWidget.style.display === 'none') {
+            extensionWidget.style.display = 'block';
+            extensionIcon.style.display = 'none'
+        }else {
+            extensionWidget.style.display = 'none'
+            extensionIcon.style.display = 'block';
+        }
 });
 
 document.body.addEventListener('click', () => {
     extensionWidget.style.display = 'none';
+    extensionIcon.style.display = 'block';
+})
+
+extensionWidget.addEventListener('click', (e) => {
+    e.stopPropagation();
 })
